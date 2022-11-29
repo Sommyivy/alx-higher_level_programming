bb0803d3f981f75ac626d4f41a337787ae1bbed6
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-11-29 08:23:27 -0800

    sommy

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last = number % 10 if number >= 0 else ((-number % 10) * -1)
 if last > 5 and last != 0:
     print("Last digit of", number, "is", last, "and is greater than 5")
-elif last_digit == 0:
+elif last == 0:
     print("Last digit of", number, "is", last, "and is 0")
 else:
     print("Last digit of", number, "is", last, "and is less than 6 and not 0")
