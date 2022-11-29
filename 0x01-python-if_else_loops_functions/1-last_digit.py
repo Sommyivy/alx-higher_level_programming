2f0f8e82ca13998b047d6d5b9e622773bff1bc71
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-11-29 08:18:55 -0800

    sommy

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,5 @@ if last_digit > 5 and last_digit != 0:
 elif last_digit == 0:
     print("Last digit of", number, "is", last_digit, "and is 0")
 else:
-    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", last_digit, "and is less than 6
+    and not 0")
