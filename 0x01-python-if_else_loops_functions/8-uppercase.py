f28ee9c07489e34d1a4dfd2f06287d2590f95cc3
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-11-30 13:31:00 -0800

    otilo

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,8 @@ def uppercase(str):
     for alphabet in str:
         low = ord(alphabet)
         jb = chr(ord(alphabet) - 32)
-
         if low >= 97 and low <= 123:
             print("{}".format(jb), end="")
         else:
             print("{}".format(alphabet), end="")
-                                                                          
+     display("\n")                                                                     
