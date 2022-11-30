ffc76a10d10b189a6cceb814481b0569a2786053
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-11-30 12:45:00 -0800

    otilo

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/phynon3
+def uppercase(str):
+    for alphabet in str:
+        low = ord(alphabet)
+        if low >= 97 and low <= 123:
+            print(chr(ord(alphabet) - 32), end="")
+        else:
+            print(alphabet, end="")
+    print("")
+                                                                          
