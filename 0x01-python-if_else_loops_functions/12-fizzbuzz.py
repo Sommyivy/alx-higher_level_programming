1071f7a050e1898d7ef831fac9cc1b0d6e81c2dd
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-11-30 04:48:42 -0800

    otilo

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range (1, 101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif  i % 3 != 0 and i % 5 != 0:
+        elif i % 3 != 0 and i % 5 != 0:
             print(i, end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
