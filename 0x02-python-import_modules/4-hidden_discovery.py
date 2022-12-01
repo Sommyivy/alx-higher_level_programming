da1b1ffeba64490e0ecdad5f043b809718b040a3
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-12-01 14:06:30 -0800

    sommy

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name.startswith("_"):
+    for s in dir(hidden_4):
+        if s.startswith("_"):
             break
         else:
             print((name))
