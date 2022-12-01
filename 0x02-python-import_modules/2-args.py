1fe61b0cda953d794f24cb46cca97afb5ce4c2a5
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-12-01 10:35:47 -0800

    sommy

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ import sys
 my_list = sys.argv
 jb = len(my_list)-1
 if jb == 1:
-    print("{}: argument".format(len(my_list)-1))
+    print("{} argument:".format(len(my_list)-1))
 else:
-    print("{}: arguments".format(len(my_list)-1))
+    print("{}: arguments:".format(len(my_list)-1))
 for item in my_list:
     indx = my_list.index(item)
     if indx != 0:
