d87fc313a62bea9193fbd2584308a16a954bc6fd
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-12-05 18:02:51 -0800

    sommy

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+
     len_a = len(tuple_a)
     len_b = len(tuple_b)
     if len_a == 0:
@@ -12,8 +13,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[0]
         a2 = tuple_a[1]
     if len_b == 0:
-         b1 = 0
-         b2 = 0
+        b1 = 0
+        b2 = 0
     elif len_b == 1:
         b1 = tuple_b[0]
         b2 = 0
@@ -22,4 +23,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b2 = tuple_b[1]
     new_tuple = (a1 + b1, a2 + b2)
     return (new_tuple)
-
