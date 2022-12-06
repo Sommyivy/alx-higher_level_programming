2eb886dac9bfbb65ecd284e54bb4e5f94daf2d4d
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-12-05 19:13:39 -0800

    sommy

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for items in matrix:
         for j in items:
-            print("{:d}".format(J), end=" ")
+            print("{:d}".format(j), end=" ")
         print()
