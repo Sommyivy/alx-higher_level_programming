4ff824f28fa0ac4615ccb2efdcb43e2c09ffb0c3
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-12-19 17:40:15 -0800

    sommy

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    total = 0
     for i in range(x):
         try:
-            print("{}".format(x), end="")
+            print("{}".format(my_list[i]), end="")
+            total = total + 1
         except:
             break
     print("")
-    return
+    return(total)
