006bd70aada6d0bac4416ed625a19e9418d21a03
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-12-19 18:38:06 -0800

    sommy

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     total = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             total = total + 1
-        except (ValueError):
-            break
+        except (TypeError, ValueError):
+            continue
     print("")
     return(total)
