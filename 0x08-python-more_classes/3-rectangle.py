98ec53835e21c59958e136e2debb043b342cea50
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2023-01-02 16:16:15 -0800

    sommy

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,15 +52,15 @@ class Rectangle:
             return 0
         return ((2 * self.__height) + (2 * self.__width))
 
-    def __str__():
+    def __str__(self):
         """returns printable
 
         characters for the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return("")
-        rec = []
-        for i in range self.__height:
+        rect = []
+        for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-            return ("".join(rect))
+        return ("".join(rect))
