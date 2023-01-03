38579373127be6aa212a20df37cbc901229e139c
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2023-01-02 16:59:42 -0800

    sommy

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ initializing the call"""
 
-number_of_instances = 0
-
 
 class Rectangle:
     """ Defining Rectangle"""
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
 
